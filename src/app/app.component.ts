e4856f1f9a6e50a1e65602c8323fdbac6fb2d073
laurenwelborn <lulu.welborn@gmail.com>
2017-08-30 18:55:41 -0600

    updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,20 +13,23 @@ class quote {
 })
 
 export class AppComponent {
+	updateText;
+	updateAuthor;
 	newQuote: quote;
 	constructor() {
 		this.newQuote = new quote();
 	}
 
 	comments = [
-     {text:"first comment!", author: ''},
-     {text: 'nice work!', author: ''},
-     {text: 'I would also like to congratulate you!', author: ''}
+     {update: false, text:"first comment!", author: ''},
+     {update: false, text: 'nice work!', author: ''},
+     {update: false, text: 'I would also like to congratulate you!', author: ''}
 	];
 
 	onSubmit(text) {
 		if (this.newQuote) {
 			var entry = {
+				'update': false,
 				'text': this.newQuote.text,
 				'author': this.newQuote.author
 			};
@@ -38,9 +41,11 @@ export class AppComponent {
 		this.comments.splice(i, 1);
 	}
 
-	// onUpdate() {
-	// 	this.comments.update();
-	// }
+	onUpdate(i) {
+		this.comments[i].update = false;
+		this.comments[i].text = this.updateText;
+		this.comments[i].author = this.updateAuthor;
+	}
 }
 
 
